53a5d4cb7dda6bbdbe1c649ecb2521ee870f5f68
gorej1234 <66222948+gorej1234@users.noreply.github.com>
2020-07-27 20:42:27 -0400

    Add files via upload

--- html/Home_Standard.php
+++ html/Home_Standard.php
@@ -18,48 +18,44 @@ if ( isset( $_SESSION['user_id'] ) ) {
 
 
 	<div class="nav">
-		<a class="selected" href='https://localhost/SeniorProject/html/Home_Standard.php'>Home</a>
+		<a class="selected" href='https://localhost/SeniorProject/html/Home_Standard.html'>Home</a>
   		<a href='https://localhost/SeniorProject/html/Incidents.php'>Incidents</a>
   		<a href='https://localhost/SeniorProject/html/Ticket.html'>Report New Incident</a>
-  			<form action= "Search.php" method="GET">
-  				<input type="submit" name= "submit"
-  				class="submit" value="GO">
-  				<input type="text" name= "search"
-  				class="search" placeholder="Search">
-  				
-  			</form>
+  		<input class="search" type=text placeholder="Search">
 	</div>
+	<br><br>
 
 	<div class="News">
 	<br><br>
 
 
-	<h3>Cybersecurity News and Tips</h3>
-	<h4>Some friendly Reminders:</h4>
+	<h3>CyberSecurity News and Tips: </h3>
 	<p>
 		- Always report any suspicous activity
-		<br>
+		<br><br>
 		- Bring any unidentified USB sticks to the IT Department
-		<br>
+		<br><br>
 
 		- Never let an unrecognized employee into the building without verifying their identity
-		<br>
-		- Stay up to date on your general CyberSecurity training
-		<br>
+		<br><br>
+		- Stay up to date on your general cybersecurity training
+		<br><br>
 	</p>
 
+
+	<br><br>
 	<h3>Noteable Breaches in the last year: </h3>
 	<p>
 		
-		<h5>November 22, 2019: T-Mobile</h5>
+		November 22, 2019: T-Mobile: <br>
 		 Over one million records compromised containing consumer data. No financial data was obtained.
 
 		<br><br>
-		<h5>November 16, 2019: Disney+</h5>
+		November 16, 2019: Disney+: <br>
 		Disney Plus subscribers locked out of their accounts. Subscriptions made available on the dark web at reduced prices.
 		<br><br>
 
-		<h5>September 27, 2019: DoorDash</h5>
+		September 27, 2019: DoorDash: <br>
 		4.9 million customers and employees exposed. Both personal and account information obtained in the breach.
 
 
